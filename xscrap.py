b3acabe3bed6b93beefd0565c559325b9c8d0613
Gabriel Herrod <gabrielhdezrodriguez@gmail.com>
2023-06-11 22:25:55 -0400

    feat: Added CLI tool to use the API

--- xscrap.py
+++ xscrap.py
@@ -5,12 +5,14 @@ import roles.client as client
 import roles.dispatcher as dispatcher
 import roles.worker as worker
 import roles.api as api
+import roles.api_cli as api_cli
 
 # Roles register
 ROLES = {
     'client': client,
     'dispatcher': dispatcher,
     'worker': worker,
+    'cli': api_cli,
     'api': api
 }
 

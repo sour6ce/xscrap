7de7cad8059848804d19f26908fd5083bb35020c
Gabriel Herrod <gabrielhdezrodriguez@gmail.com>
2023-07-08 11:41:04 -0400

    fix: API CLI fix

--- roles/api_cli.py
+++ roles/api_cli.py
@@ -2,6 +2,7 @@ import requests
 from .api import Batch
 import json
 
+
 class start():
     '''
     CLI tool to use the API from the XSCRAP system.
@@ -11,21 +12,22 @@ class start():
 
        /reset
     '''
-    def fetch(self,api:str,*urls:str):
+
+    def fetch(self, api: str, *urls: str):
         '''
         Given a list of URLs, print for each one a JSON that 
         stores the HTML body and the status code returned. 580 is a special 
         status code for error at DNS or connection start.
         '''
-        response:requests.Response = requests.post(f'{api}/fetch', Batch(urls=list(urls)), stream=True)
+        response: requests.Response = requests.post(
+            f'{api}/scrap', data=Batch(urls=list(urls)).json(), stream=True)
         for line in response.iter_lines():
-            if line:  # filter out keep-alive new lines
+            if line and 'ping' not in line:  # filter out keep-alive new lines
                 print(line)
 
-    def reset(self,api:str,*urls):
+    def reset(self, api: str, *urls):
         '''
         Given a list of URLs, clean the result stored in cache from all of 
         them.
         '''
         requests.post(f'{api}/reset', Batch(urls=list(urls)))
-    
